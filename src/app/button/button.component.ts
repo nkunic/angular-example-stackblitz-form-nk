543fbe435e7c11573b4482af53d75a85c866f661
nkunic <45877400+nkunic@users.noreply.github.com>
2023-05-02 18:26:46 +0200

    3 files modified

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
+  @Input() disabled = false;
+  @Output() clickEvent: EventEmitter<any> = new EventEmitter<any>();
+
   constructor() {}
 
   ngOnInit() {}
+
+  onClick(event: any): void {
+    if (!this.disabled) {
+      this.clickEvent.emit(event);
+    }
+  }
 }
