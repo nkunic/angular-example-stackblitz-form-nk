93feeddb3202e4b8668269578ae7266d8eb26419
nkunic <45877400+nkunic@users.noreply.github.com>
2023-05-03 12:04:00 +0200

    3 files modified

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -6,24 +7,18 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./input.component.scss'],
 })
 export class InputComponent {
+  @Input() formGroup!: FormGroup;
+  @Input() inputFormControlName!: any;
+
   @Input() inputId: string | undefined;
   @Input() inputName: string = '';
-
-  @Input() inputSize: 'small' | 'medium' | 'large' = 'medium';
-  @Input() inputStatus: 'default' | 'success' | 'danger' = 'default';
 
   @Input() inputType: 'text' | 'number' | undefined;
 
   inputValue: string | undefined;
 
-  @Input() inputMinLength: string | number | null = null;
-  @Input() inputMaxLength: string | number | null = null;
+
 
   @Input() inputPlaceholder: string | undefined;
 
-  @Input() inputRequiredField: boolean = false;
-  @Input() inputFormControlName: string = '';
-
-  @Input() inputIsDisabled: boolean = false;
-  @Input() tabIndex: number | undefined;
 }
